c1d40b7efd7d4d870b7dacd42353fa28427d0083
gaoyongxin <gaoyongxing@anji-plus.com>
2018-11-14 17:30:16 +0800

    配置kotlin

--- app/src/main/java/com/yongxingg/mydemo/mvvm/UserModel.java
+++ app/src/main/java/com/yongxingg/mydemo/mvvm/UserModel.java
@@ -1,7 +1,9 @@
 package com.yongxingg.mydemo.mvvm;
 
+import android.arch.lifecycle.ViewModel;
+
 /**
  * Created by gaoyongxing on 2018-11-14.
  */
-public class UserModel {
+public class UserModel extends ViewModel {
 }
